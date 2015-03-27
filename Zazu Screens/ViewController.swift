7fc49504f16c5e7b48283229fc7cf9dbb48f1062
gokul2005 <gokul@Gokuls-MacBook-Pro.local>
2015-03-27 00:15:02 -0400

    Use case 1

--- Zazu Screens/ViewController.swift
+++ Zazu Screens/ViewController.swift
@@ -51,12 +51,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         //Refresh events array!
         events = Array<Event>()
         
-        var event1 = Event(name: "Design Discussion", desc: "Hey guys! Let's go over our design!", duration: "45 mins", email: "snbinjie@gmail.com", creator_name: "Binjie Sun", attendees: "You (Gokul), Saajan Shridhar, Song Zheng, Apoorva Verlekar", sd: "Mar 20, 2015, 2:00 PM", ed: "Mar 22, 2015, 5:00 PM", dd: "", status: "new", image: "binjie.jpg")
-        var event2 = Event(name: "Dinner at Six Feet Under", desc: "Let's catch up over dinner.", duration: "2 hours", email: "gokul2005@gmail.com", creator_name: "Gokul Raghuraman",attendees: "Binjie Sun, Saajan Shridhar", sd: "Mar 24, 2015, 6:00 PM", ed: "Mar 25, 2015, 8:00 PM", dd: "", status: "accepted", image: "gokul.jpg")
-        var event3 = Event(name: "Touch base with Binjie / Song", desc: "Updates on our MAS project", duration: "30 mins", email: "gokul2005@gmail.com", creator_name: "Gokul raghuraman",attendees: "Binjie Sun, Song Zheng", sd: "Mar 18, 2015, 6:00 PM", ed: "Mar 20, 2015, 6:00 PM", dd: "", status: "declined", image: "saajan.jpg")
-        var event4 = Event(name: "MAS Architecture Discussion", desc: "Let's meet up to go over our app's architecture", duration: "2 hours", email: "szheng60@gmail.com", creator_name: "Song Zheng",attendees: "Gokul Raghuraman, Binjie Sun, Saajan Shridhar, Apoorva Verlekar", sd: "Mar 12, 2015, 4:00 PM", ed: "Mar 14, 2015, 6:00 PM", dd: "", status: "passed", image: "song.jpg")
-        var event5 = Event(name: "AI Final Project", desc: "Discussion of final project submission", duration: "2 hours", email: "gokul2005@gmail.com", creator_name: "Gokul Raghuraman",attendees: "Sachin Gupta, Saajan Shridhar", sd: "Dec 4, 2014, 3:00PM", ed: "Dec 5, 2014, 2:00PM", dd: "", status: "passed", image: "gokul.jpg")
-        var event6 = Event(name: "Business Proposal Discussion", desc: "", duration: "", email: "", creator_name: "Gokul Raghuraman",attendees: "Adam Smith, Rohan Gupta", sd: "Nov 15 , 2014, 5:00PM", ed: "", dd: "", status: "passed", image: "gokul.jpg")
+        var event1 = Event(name: "Design Discussion", desc: "Hey guys! Let's go over our design!", duration: "45 mins", email: "snbinjie@gmail.com", creator_name: "Binjie Sun", attendees: "You (Gokul), Saajan Shridhar, Song Zheng, Apoorva Verlekar", sd: "Mar 20, 2015, 2:00 PM", ed: "Mar 22, 2015, 5:00 PM", dd: "Mar 19, 2015, 5:00 PM", status: "new", image: "binjie.jpg")
+        
+        var event2 = Event(name: "Dinner at Six Feet Under", desc: "Let's catch up over dinner.", duration: "2 hours", email: "gokul2005@gmail.com", creator_name: "Gokul Raghuraman",attendees: "Binjie Sun, Saajan Shridhar", sd: "Mar 24, 2015, 6:00 PM", ed: "Mar 25, 2015, 8:00 PM", dd: "Mar 23, 2015, 6:00 PM", status: "accepted", image: "gokul.jpg")
+        
+        var event3 = Event(name: "Touch base with Binjie / Song", desc: "Updates on our MAS project", duration: "30 mins", email: "gokul2005@gmail.com", creator_name: "Gokul raghuraman",attendees: "Binjie Sun, Song Zheng", sd: "Mar 18, 2015, 6:00 PM", ed: "Mar 20, 2015, 6:00 PM", dd: "Mar 17, 2015, 6:00 PM", status: "declined", image: "saajan.jpg")
+        
+        var event4 = Event(name: "MAS Architecture Discussion", desc: "Let's meet up to go over our app's architecture", duration: "2 hours", email: "szheng60@gmail.com", creator_name: "Song Zheng",attendees: "Gokul Raghuraman, Binjie Sun, Saajan Shridhar, Apoorva Verlekar", sd: "Mar 12, 2015, 4:00 PM", ed: "Mar 14, 2015, 6:00 PM", dd: "Mar 11, 2015, 6:00 PM", status: "passed", image: "song.jpg")
+        
+        var event5 = Event(name: "AI Final Project", desc: "Discussion of final project submission", duration: "2 hours", email: "gokul2005@gmail.com", creator_name: "Gokul Raghuraman",attendees: "Sachin Gupta, Saajan Shridhar", sd: "Dec 4, 2014, 3:00PM", ed: "Dec 5, 2014, 2:00PM", dd: "Dec 2, 2014, 3:00PM", status: "passed", image: "gokul.jpg")
+        
+        var event6 = Event(name: "Business Proposal Discussion", desc: "", duration: "", email: "", creator_name: "Gokul Raghuraman",attendees: "Adam Smith, Rohan Gupta", sd: "Nov 15 , 2014, 5:00PM", ed: "Nov 18 , 2014, 5:00PM", dd: "Nov 14 , 2014, 5:00PM", status: "passed", image: "gokul.jpg")
         
         
         if self.newEvent != nil
